6cd47534ddc98b48985d38fa2646af3fa570a3e9
codey4k <codey4k@gmail.com>
2017-10-21 23:17:24 +0000

    adding images folder / adding logo at menu and header section

--- header.php
+++ header.php
@@ -11,12 +11,12 @@
     <!-- Menu bar-->
             <div class="menu">
                 <div class="menu__container">
-                    <h2>Logo</h2>
+                    <img src=" <?php echo get_stylesheet_directory_uri()?>/images/y4k.png" alt="logo y4k" class="menu__logo">
                 
                     <h2>icono</h2>
                 </div>
             </div>
             
             <header class="header">
-                
+                <img src="<?php echo get_stylesheet_directory_uri()?>/images/codey4klogogrande.png" alt="logo" class="header__logo"/>
             </header>
