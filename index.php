d4ec5d82a0fbf7472f7bb6a75a951df2ec30b1b8
codey4k <codey4k@gmail.com>
2017-10-21 23:52:10 +0000

    adding features section

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php get_header()?>
 
-<?php 
-    echo 'CodeY4k'
-?>
+<section class="features">
+    <div class="features__container">
+        <h2 class="features__title">Featues</h2>
+        <p class="features__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+        magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+        quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+        ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+</section>
 
 <?php get_footer()?>
