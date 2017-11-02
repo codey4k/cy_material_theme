eadca09ebad5ad54e1d856078a00a2cf12ecfcd1
codey4k <codey4k@gmail.com>
2017-11-02 20:29:46 +0000

    custom single.php

--- single.php
+++ single.php
@@ -1,18 +1,24 @@
 <?php get_header(); ?>
     
 <?php while( have_posts() ) : the_post(); ?>
-    <figure>
+    <section class="post__header">
         <?php
             echo get_the_post_thumbnail( 
                 $post_id, 
                 'full', 
-                array('class' => 'post__thumbnail', 'alt' => get_the_title() )
+                array('class'=>'post__thumbnail-header', 'alt'=> get_the_title() )
             );
         ?>
-    </figure>
-    <div>
-        <?php the_content(); ?>
-    </div>
+        <?php the_title('<h3 class="post__title-header">', '</h3>'); ?>
+    </section>
+    
+    <section class="post__content">
+        <div class="post__content-center">
+            <div class="post__content-bg">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </section>
     
 <?php endwhile;?>
     
