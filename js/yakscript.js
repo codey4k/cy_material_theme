8d17f341f140ccf79a73967f28c3527390bcac4b
codey4k <codey4k@gmail.com>
2018-01-08 17:55:37 +0000

    update menu bar

--- js/yakscript.js
+++ js/yakscript.js
@@ -3,9 +3,17 @@ window.addEventListener('scroll',function(){
         menu = document.getElementById('menu')
         
     if(scroll > 300){
-        menu.style.background = "rgba(34, 34, 34, 0.8)"
+        Object.assign(menu.style,{
+            height : '60px',
+            background : 'rgba(34, 34, 34, 0.8)'
+        })
+        // menu.style.background = "rgba(34, 34, 34, 0.8)"
     }else{
-        menu.style.background = "transparent"
+        Object.assign(menu.style,{
+            height : '80px',
+            background : 'transparent'
+        })
+        // menu.style.background = "transparent"
     }
     
 })
