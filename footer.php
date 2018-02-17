19b76436613f712475d04757e83a23f7f7b3b5db
codey4k <codey4k@gmail.com>
2018-02-17 22:52:42 +0000

    Define the structure of the footer

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
-            <footer class=footer></footer>
+            <footer class=footer>
+                <div class=footer__container>
+                    <div class="footer__content">
+                        <div class="footer__left"></div>
+                        <div class="footer__right"></div>
+                    </div>
+                    
+                    <div class="footer__copyright">
+                        <p>Copyright &copy; <span>CodeY4k</span></p>
+                    </div>
+                </div>
+            </footer>
         </div>
         <?php wp_footer(); ?>
     </body>
