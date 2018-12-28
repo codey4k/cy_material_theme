db8dba7cb037996daab16d7e6bcce6011f27caff
codey4k <codey4k@gmail.com>
2018-12-28 20:37:29 +0000

    update 28/12/2018

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
     <img src="<?php echo get_stylesheet_directory_uri()?>/images/codey4klogogrande.png" alt="logo" class="header__logo"/>
 </header>
 
-<section class="features">
-    <div class="features__container">
-        <div class="features__bg"></div>
-        <h1 class="features__title">Features</h1>
-        <p class="features__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-        sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-        magna aliquam erat volutpat. 
-        </p>
-    </div>
-</section>
+<!--<section class="features">-->
+<!--    <div class="features__container">-->
+<!--        <div class="features__bg"></div>-->
+<!--        <h1 class="features__title">Features</h1>-->
+<!--        <p class="features__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,-->
+<!--        sed diam nonummy nibh euismod tincidunt ut laoreet dolore-->
+<!--        magna aliquam erat volutpat. -->
+<!--        </p>-->
+<!--    </div>-->
+<!--</section>-->
 
 
 <section class="main">
